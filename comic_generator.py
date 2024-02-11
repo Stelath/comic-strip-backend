fc54b7d958958b9bc8da125f9431438821be4a5e
Elan456 <telan4892@gmail.com>
2024-02-11 07:01:41 -0500

    batched diffusion

--- comic_generator.py
+++ comic_generator.py
@@ -1,69 +1,81 @@
 from story_generator.story_prompter import ComicBookPrompter
-from fine_tuned_diffusion.diffusion import get_image
+from fine_tuned_diffusion.diffusion import get_images
 
 import os
 import time
-from multiprocessing import Pool
 
-def process_prompt(data):
-    i, prompt = data
-    print(f"Gathering image for frame {i}")
-    image = get_image(prompt)
-    print(f"Got image: {i}")
-    
-    return image
 
-class ComicGenerator():
+class ComicGenerator:
     def __init__(self, prompt, job_id) -> None:
         self.start_time = time.time()
         self.end_time = None
         self.time_taken = None
         self.user_prompt = prompt
         self.job_id = job_id
-        
+
         self.progress = 0
         self.current_state = "Building Story"
-        
+
         self.story_title = ""
         self.frames = []
-    
+
     def generate(self):
         output_dir = os.path.join('generated_comics', self.job_id)
         os.makedirs(output_dir, exist_ok=True)
         os.makedirs(os.path.join(output_dir, 'frames'), exist_ok=True)
-        
+
         story = ComicBookPrompter(self.user_prompt, num_frames=8)
         self.story_title = story.title
         self.current_state = "Story built"
         self.progress = 0.2
-        
+
         # Write story to file
         with open(os.path.join(output_dir, 'story_output.txt'), "w") as file:
             file.write(story.__str__())
 
-        # Get images for each frame using multiprocessing
-        with Pool() as p:
-            # Process the prompts and store them in frame_images using multiprocessing
-            # The order of the frames is important, so we use enumerate to keep track of the index
-            frame_images = p.map(process_prompt, enumerate(story.frame_diffusion_prompts))
-        
+        batch_size = 3
+
+        # Break the diffusion prompts into batches of at most batch_size prompts
+        batches = [story.frame_diffusion_prompts[i:i + batch_size] for i in range(0, len(story.frame_diffusion_prompts), batch_size)]
+        frame_images = []
+        for batch in batches:
+            print("Getting images for batch", batch)
+            print("number of prompts in batch", len(batch))
+            frame_images.extend(get_images(batch))
+            self.progress += 0.6 / len(batches)
+
+
+
         self.current_state = "Generated Images"
         self.progress = 0.8
-        
+
         # Wait to import so multiprocessing doesn't by default
         from text_bubble_adder.text_bubble import add_text_bubbles
-        
+
         for i, image in enumerate(frame_images):
             image = add_text_bubbles(image, story.frames[i])
-            
+
             frame_name = f"frame_{i:03}.png"
             # print(f"Saving frame {i} to {os.path.join(output_dir, 'frames', frame_name)}")
             image.save(os.path.join(output_dir, 'frames', frame_name))
             self.frames.append(os.path.join('/api', 'images', self.job_id, frame_name))
-        
+
         self.current_state = "Text Bubbles Added"
         self.progress = 1.0
-        
+
         self.end_time = time.time()
         self.time_taken = self.end_time - self.start_time
         self.current_state = "Completed"
+
+
+if __name__ == "__main__":
+    # Test the comic generator
+    comic = ComicGenerator("Astronaut riding a horse", "test_job")
+    comic.generate()
+    print(comic.story_title)
+    print(comic.frames)
+    print(comic.time_taken)
+    print(comic.current_state)
+    print(comic.progress)
+    print(comic.user_prompt)
+    print(comic.job_id)
--- fine_tuned_diffusion/diffusion.py
+++ fine_tuned_diffusion/diffusion.py
@@ -2,30 +2,32 @@ import requests
 import io
 import time
 from PIL import Image
+import base64
 
 # API_URL = "https://api-inference.huggingface.co/models/Stelath/textual_inversion_comic_strip_test"
 # key = open("huggingface_diffusion/hf_key", "r").read()
 # headers = {"Authorization": f"Bearer {key}"}
 
 
-API_URL = "https://zlie2z38iazjx0cm.us-east-1.aws.endpoints.huggingface.cloud/"
+API_URL = "https://stsunag27z4o1dpn.us-east-1.aws.endpoints.huggingface.cloud/"
 headers = {
-    "Accept" : "image/png",
+    "Accept": "application/json",
     "Authorization": "Bearer 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",
     "Content-Type": "application/json"
 }
 
 
-
-def get_image(prompt):
+def get_images(prompts):
     """
     Given a prompt, get an image from the model
     :param prompt: A string
     :return: A PIL Image
     """
-    print("Using Alex's Diffusion model")
+
+    prompts = ["An illustration in <comic-strip> style of " + prompt for prompt in prompts]
+
     payload = {
-        "inputs": prompt + ", in the style of <comic-strip>",
+        "inputs": prompts,
         "negative_prompt": "text bubble, black and white, low saturation, blurry, low res, photorealistic, low contrast"
     }
     good = False
@@ -35,16 +37,36 @@ def get_image(prompt):
             response.raise_for_status()  # What does this line do?  -> This line raises an HTTPError if the HTTP request returned an unsuccessful status code.
             good = True
         except requests.exceptions.HTTPError as err:
-            print("Error: " + str(err) + " Prompt: " + prompt)
+            print("Error: " + str(err) + " Prompts: " + str(prompts))
             time.sleep(5)
 
-    image_bytes = response.content
-    image = Image.open(io.BytesIO(image_bytes))
-    return image
+    response = response.json()
+    # print("response: ", response.keys())
+    image_prompt_pairs = []
+    from_server_image_prompt_pairs = response["outputs"]
+    for data in from_server_image_prompt_pairs:
+        image_string = data["image"]  # This is a jpeg image encoded to string utf-8 from base 64
+        image_bytes = base64.b64decode(image_string)
+        image = Image.open(io.BytesIO(image_bytes))
+
+        prompt = data["prompt"]
+        image_prompt_pairs.append((image, prompt))
+
+    images = []
+
+    # Fill the images list with the images from the image_prompt_pairs
+    # such that the order of the images matches the order of the prompts originally passed in
+
+    for prompt in prompts:
+        for image, image_prompt in image_prompt_pairs:
+            if prompt == image_prompt:
+                images.append(image)
+                break
+    return images
+
 
 if __name__ == "__main__":
-    prompt = "strawberry on a beach"
-    image = get_image(prompt)
-    # Save the image
-    image.save("output.png")
-    image.show()
+    prompts = ["strawberry on a beach", "lettuce on an iceberg", "carrot in a garden"]
+    images = get_images(prompts)
+    for image in images:
+        image.show()
